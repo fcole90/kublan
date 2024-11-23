e63e2d3eb8eb83e332bed09f0f44ee1ef1e96f3b
Fabio Colella <fcole90@gmail.com>
2024-11-23 21:02:03 +0100

    Override base methods in RectangleNode and allow color

--- packages/examples/pong/src/nodes/RectangleNode.ts
+++ packages/examples/pong/src/nodes/RectangleNode.ts
@@ -1,20 +1,26 @@
 import { Rectangle, RectangleInitializer } from "../primitives/Rectangle";
-import { Vector2D } from "../primitives/Vector2D";
+import { Vector2D, Vector2DInitializer } from "../primitives/Vector2D";
 import { Renderer } from "../render/Renderer";
 import { BaseNode, BaseNodeConfig } from "./BaseNode";
 
 export interface RectangleNodeConfig extends BaseNodeConfig {
-  size: Vector2D
+  size: Vector2DInitializer
+  color?: string
 }
 
 export class RectangleNode extends BaseNode {
   size: Vector2D
-  color: string = 'white'
+  color: string
 
   constructor(config: RectangleNodeConfig) {
     super(config)
     this.size = new Vector2D(config.size)
+    this.color = config.color ?? 'white'
   }
+
+  start(): void { }
+
+  update(eps: number): void { }
 
   render(renderer: Renderer) {
     renderer.drawRectangle([...this.getAbsolutePosition().toArray(), ...this.size.toArray()], this.color)
