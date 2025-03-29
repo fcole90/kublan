21f96fa3aeb1cbc0e7a7f5cbcd5a8b336372d139
Fabio Colella <fcole90@gmail.com>
2025-03-29 17:50:53 +0100

    Add intial prettier config

--- packages/config/eslintBaseConfig.mjs
+++ packages/config/eslintBaseConfig.mjs
@@ -2,11 +2,13 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 /** @typedef {import('typescript-eslint')} */
 
 export default tseslint.config(
   eslint.configs.recommended,
+  eslintPluginPrettierRecommended,
   ...tseslint.configs.strictTypeChecked,
   {
 
