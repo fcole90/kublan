dc4a0ac85c27b0248ccee56b768d75b8af78f66c
Fabio Colella <fcole90@gmail.com>
2025-03-29 17:50:43 +0100

    Fit canvas to page size

--- packages/examples/pong/index.ts
+++ packages/examples/pong/index.ts
@@ -1,60 +1,74 @@
-import { ExampleGame } from "@examples/common/types"
+import { ExampleGame } from "@examples/common/types";
 // import { kublan } from "@kublan/engine"
-import { settings } from "./src/gameSettings"
-import { Core } from "./src/engine/Core"
-import { MainScene } from "./src/scenes/main/MainScene"
+import { settings } from "./src/gameSettings";
+import { Core } from "./src/engine/Core";
+import { MainScene } from "./src/scenes/main/MainScene";
 
 interface CreateGameCanvasOptions {
-    size: { x: number, y: number }
+  size: { x: number; y: number };
 }
 
 const resetHTML = (rootElement: HTMLElement, backgroundColor: string) => {
-    document.getElementsByTagName('body')[0].setAttribute('style', `
+  document.getElementsByTagName("body")[0].setAttribute(
+    "style",
+    `
         box-sizing: border-box;
         margin: 0;
-        `)
-    rootElement.setAttribute('style', `
+        `
+  );
+  rootElement.setAttribute(
+    "style",
+    `
         background-color: ${backgroundColor};
         display: flex;
         height: 100vh;
         width: 100vw;
         margin: 0;
-        `)
-}
+        `
+  );
+};
 
-const createGameCanvas = ({ size }: CreateGameCanvasOptions): HTMLCanvasElement => {
-    const canvas = document.createElement('canvas')
-    canvas.width = size.x
-    canvas.height = size.y
-    canvas.setAttribute('style', `
+const createGameCanvas = ({
+  size,
+}: CreateGameCanvasOptions): HTMLCanvasElement => {
+  const canvas = document.createElement("canvas");
+  canvas.width = size.x;
+  canvas.height = size.y;
+  canvas.setAttribute(
+    "style",
+    `
          image-rendering: pixelated;
          background-color: red;
-        `)
+         height: 100%;
+         width: 100%;
+         max-width: fit-content;
+         max-height: fit-content;
+         flex: 1;
+        `
+  );
 
-
-    return canvas
-}
-
+  return canvas;
+};
 
 // eslint-disable-next-line @typescript-eslint/require-await
-const load: ExampleGame['load'] = async (rootElement) => {
-    resetHTML(rootElement, settings.pageBackgroundColor)
-    const gameCanvas = createGameCanvas({ size: settings.viewportSize })
-    rootElement.replaceChildren(gameCanvas)
+const load: ExampleGame["load"] = async (rootElement) => {
+  resetHTML(rootElement, settings.pageBackgroundColor);
+  const gameCanvas = createGameCanvas({ size: settings.viewportSize });
+  rootElement.replaceChildren(gameCanvas);
 
-    const ctx = gameCanvas.getContext('2d')
-    if (ctx == null) {
-        console.error('Could not load context')
-        return
-    }
+  const ctx = gameCanvas.getContext("2d");
+  if (ctx == null) {
+    console.error("Could not load context");
+    return;
+  }
 
-    const pong = new Core({
-        ctx,
-        rootScene: new MainScene()
-    })
-    pong.start()
-}
+  const pong = new Core({
+    ctx,
+    rootScene: new MainScene(),
+  });
+  pong.start();
+};
 
 export const game: ExampleGame = {
-    load
-}
+  load,
+};
